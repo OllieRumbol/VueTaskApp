9bba22dc992d36bfac587f5c0593290c9a5cd8a9
OllieRumbol <otgb97@gmail.com>
2020-11-03 09:29:04 +0000

    Adding modal

--- vue-tasks-app/src/components/Item.vue
+++ vue-tasks-app/src/components/Item.vue
@@ -1,105 +1,15 @@
 <template>
   <div class="d-inline-flex w-100">
-    <div class="dropdown w-100">
-      <!-- Task Name -->
-      <button
-        :class="button3Style"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-expanded="false"
-      >
-        {{ task.name }}
-      </button>
-      <!--JOBS MENU-->
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <!--UPDATE TASK-->
-        <div class="p-3 input-group">
-          <input
-            type="text"
-            class="form-control mr-3"
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            v-model="task.name"
-          />
-          <button
-            type="button"
-            class="btn btn-primary"
-            v-on:click.stop.prevent="editTask"
-          >
-            Save
-          </button>
-        </div>
-        <div v-show="showTaskErrorMessage" class="p-3 input-group">
-          <div class="alert alert-danger" role="alert">
-            {{ taskErrorMessage }}
-          </div>
-        </div>
-        <!--JOBS SECTION-->
-        <div>
-          <h2 class="p-3">
-            Jobs <span class="badge badge-secondary ml-1">{{ task.jobs.length }}</span>
-          </h2>
-          <!-- NEW -->
-          <table class="p-3 input-group table d-flex justify-content-center">
-            <tbody>
-              <tr>
-                <th class="text-center"><h5>Done</h5></th>
-                <th class="text-center"><h5>Name</h5></th>
-                <th class="text-center"><h5>Delete</h5></th>
-              </tr>
-              <tr v-for="(job, key) in task.jobs" v-bind:key="key">
-                <td class="text-center">
-                  <input
-                    class="mt-2"
-                    type="checkbox"
-                    v-model="job.done"
-                    id="defaultCheck1"
-                    v-on:click.stop.prevent="editJobStatus(job.id, job.done)"
-                  />
-                </td>
-                <td class="text-center">
-                  <h4>{{ job.name }}</h4>
-                </td>
-                <td class="text-center">
-                  <button
-                    class="btn btn-danger"
-                    v-on:click.stop.prevent="delJob(job.id)"
-                  >
-                    Del
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-
-          <!--ADD JOB-->
-          <div class="p-3 input-group">
-            <input
-              type="text"
-              class="form-control mr-3"
-              placeholder="New job"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-              v-model="newJob"
-            />
-            <button
-              type="button"
-              class="btn btn-success"
-              v-on:click.stop.prevent="addJob(task.id)"
-            >
-              +
-            </button>
-          </div>
-          <div v-show="showJobErrorMessage" class="p-3 input-group">
-            <div class="alert alert-danger" role="alert">
-              {{ jobErrorMessage }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!-- Task Name -->
+    <button
+      :class="button3Style"
+      type="button"
+      id="dropdownMenuButton"
+      @click="open"
+    >
+      {{ task.name }}
+    </button>
+    <Modal v-if="showModal" @close="close" :task="task" />
 
     <!--TASK MENU-->
     <div class="dropleft">
@@ -121,9 +31,13 @@
 </template>
 
 <script>
+import Modal from "./Modal";
+
 export default {
   name: "Item",
-  components: {},
+  components: {
+    Modal,
+  },
   props: {
     task: Object,
     colour: String,
@@ -136,6 +50,7 @@ export default {
       jobErrorMessage: "Cannot add empty job, please add a value",
       showTaskErrorMessage: false,
       taskErrorMessage: "Cannot have an empty task name, Please add value",
+      showModal: false,
     };
   },
   methods: {
@@ -149,58 +64,18 @@ export default {
     deleteTask: function() {
       this.$store.dispatch("deleteTask", this.task.id);
     },
-    editTask: function() {
-      this.showTaskErrorMessage = false;
-      if (this.task.name == "") {
-        this.showTaskErrorMessage = true;
-        return;
-      }
-
-      let data = JSON.stringify({
-        Id: this.task.id,
-        Name: this.task.name,
-      });
-      this.$store.dispatch("renameTask", data);
-    },
-    addJob: function(id) {
-      this.showJobErrorMessage = false;
-      if (this.newJob == "") {
-        this.showJobErrorMessage = true;
-        return;
-      }
-
-      let data = JSON.stringify({
-        TaskId: id,
-        JobName: this.newJob,
-      });
-
-      this.$store.dispatch("addJob", data);
-
-      this.newJob = "";
-    },
-    delJob: function(jobId) {
-      let data = JSON.stringify({
-        TaskId: this.task.id,
-        JobId: jobId,
-      });
-
-      this.$store.dispatch("deleteJob", data);
-    },
-    editJobStatus: function(jobId, jobStatus) {
-      let data = JSON.stringify({
-        TaskId: this.task.id,
-        JobId: jobId,
-        Done: !jobStatus,
-      });
-
-      this.$store.dispatch("editJobStatus", data);
-    },
     getStatus: function(status) {
       if (status == 2) {
         return 0;
       } else {
         return status + 1;
       }
+    },
+    close() {
+      this.showModal = false;
+    },
+    open() {
+      this.showModal = true;
     },
   },
   computed: {
---
+++ vue-tasks-app/src/components/Modal.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="blocker">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <h2 class="text-primary">Task Modal</h2>
+          </div>
+          <div class="modal-body">
+            <div>
+              <h5 class="font-weight-bold">Task Name</h5>
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control mr-3"
+                  v-model="task.name"
+                />
+                <button type="button" class="btn btn-primary" v-on:click="editTask">
+                  Save
+                </button>
+              </div>
+              <div v-show="showTaskErrorMessage" class="pt-4">
+                <div class="alert alert-danger" role="alert">
+                  {{ taskErrorMessage }}
+                </div>
+              </div>
+            </div>
+            <br />
+            <div>
+              <h5 class="font-weight-bold">Task Description</h5>
+              <div class="input-group">
+                <input type="text" class="form-control mr-3" />
+                <button type="button" class="btn btn-primary">
+                  Save
+                </button>
+              </div>
+            </div>
+            <br />
+            <div>
+              <h5 class="font-weight-bold">Task Completed Date</h5>
+
+              <br />
+            </div>
+            <div>
+              <h5 class="font-weight-bold">Jobs <span class="badge badge-secondary ml-1">{{ task.jobs.length }}</span></h5>
+              <table class="table justify-content-center">
+                <tbody>
+                  <tr>
+                    <th class="text-center">
+                      <h5 class="font-weight-bold">Done</h5>
+                    </th>
+                    <th class="text-center">
+                      <h5 class="font-weight-bold">Name</h5>
+                    </th>
+                    <th class="text-center">
+                      <h5 class="font-weight-bold">Delete</h5>
+                    </th>
+                  </tr>
+                  <tr v-for="(job, key) in task.jobs" v-bind:key="key">
+                    <td class="text-center">
+                      <input
+                        class="mt-2"
+                        type="checkbox"
+                        v-model="job.done"
+                        id="defaultCheck1"
+                        v-on:click.stop.prevent="
+                          editJobStatus(job.id, job.done)
+                        "
+                      />
+                    </td>
+                    <td class="text-center">
+                      <h4>{{ job.name }}</h4>
+                    </td>
+                    <td class="text-center">
+                      <button
+                        class="btn btn-danger"
+                        v-on:click.stop.prevent="delJob(job.id)"
+                      >
+                        Del
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="input-group">
+                <input type="text" class="form-control mr-3" v-model="newJob" />
+                <button type="button" class="btn btn-success" v-on:click="addJob(task.id)">
+                  Save
+                </button>
+              </div>
+              <div v-show="showJobErrorMessage" class="pt-4">
+                <div class="alert alert-danger" role="alert">
+                  {{ jobErrorMessage }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button
+              class="float-right btn btn-primary"
+              placeholder="New Job"
+              @click="close"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "../style/modal.css";
+
+export default {
+  name: "Modal",
+  components: {},
+  props: {
+    task: Object,
+  },
+  mounted() {},
+  data() {
+    return {
+      newJob: "",
+      showJobErrorMessage: false,
+      jobErrorMessage: "Cannot add empty job, please add a value",
+      showTaskErrorMessage: false,
+      taskErrorMessage: "Cannot have an empty task name, Please add value",
+    };
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+
+    editTask: function() {
+      this.showTaskErrorMessage = false;
+      if (this.task.name == "") {
+        this.showTaskErrorMessage = true;
+        return;
+      }
+
+      let data = JSON.stringify({
+        Id: this.task.id,
+        Name: this.task.name,
+      });
+      this.$store.dispatch("renameTask", data);
+    },
+    addJob: function(id) {
+      this.showJobErrorMessage = false;
+      if (this.newJob == "") {
+        this.showJobErrorMessage = true;
+        return;
+      }
+
+      let data = JSON.stringify({
+        TaskId: id,
+        JobName: this.newJob,
+      });
+
+      this.$store.dispatch("addJob", data);
+
+      this.newJob = "";
+    },
+    delJob: function(jobId) {
+      let data = JSON.stringify({
+        TaskId: this.task.id,
+        JobId: jobId,
+      });
+
+      this.$store.dispatch("deleteJob", data);
+    },
+    editJobStatus: function(jobId, jobStatus) {
+      let data = JSON.stringify({
+        TaskId: this.task.id,
+        JobId: jobId,
+        Done: !jobStatus,
+      });
+
+      this.$store.dispatch("editJobStatus", data);
+    }
+  },
+  computed: {},
+};
+</script>
